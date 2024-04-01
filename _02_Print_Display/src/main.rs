a9722a25652c92e764c1806c191ff7837e85da91
vpin09 <sainivipin1998@gmail.com>
2024-04-01 15:01:38 +0530

    mutability

--- _02_Print_Display/src/main.rs
+++ _02_Print_Display/src/main.rs
@@ -1,5 +1,23 @@
 fn main() {
     println!("Hello, world! number {} and {}", 8,9);
+    println!("number {}",number());
+   let multi= multiply(10, 12);
+   println!("multi======{}",multi);
+   let my_number = {
+    let second_number = 8;
+        second_number + 9 
+    };
+    let doesnt_print=();
+    println!("this wil noit print: {}", i8::MAX);
 
+    println!("My number is: {}", my_number);
 
+}
+fn number() ->i32 {
+    8
+}
+fn multiply(number_one:i32, number_two:i32)->i32 {
+    let res=number_one*number_two;
+    println!("Result multiply ={}",res);
+    return  res;
 }
