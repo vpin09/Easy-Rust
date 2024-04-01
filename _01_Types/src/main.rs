23aacf0a4a8d4f434787c4cf6176b010301dad31
vpin09 <sainivipin1998@gmail.com>
2024-04-01 12:58:46 +0530

    print

--- _01_Types/src/main.rs
+++ _01_Types/src/main.rs
@@ -1,9 +1,20 @@
+use core::slice;
+
 fn main() {
 
     let first_letter='A';
     let space =' ';
     let other_lang_char='Ꮔ';
     let cat_face= '😺';
+    let small_number:u8=10;
+    let small_number=10u8;
+    let big_number=10_00_000_i32;
+    let float=6.;
+    let my_float:f64=5.0;
+    let my_other_float:f32=7.5;
+    let third_float=my_float + my_other_float as f64;
+
+
 
     let my_num=100;
     let my_number:u8=100; 
@@ -16,7 +27,12 @@ fn main() {
 
     let slice="Hello";
     println!("Slice is {} bytes", slice.len());
-    
+    println!("Slice is {} bytes and in chars {}", slice.len(),slice.chars().count());
+    let slice2="안녕";
+    println!("Slice2 is {} bytesand in chars {}", slice2.len(), slice2.chars().count());
+
+
+
 
 
 
---
+++ _02_Print_Display/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world! number {} and {}", 8,9);
+
+
+}
